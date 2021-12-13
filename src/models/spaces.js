29d474ffef55129d7d6afde24f3bf501dfc8e2cf
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-12-13 23:14:59 +0530

    DB server connected

---
+++ src/models/spaces.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+ 
+module.exports = new mongoose.Schema({
+    name : {type: String, required: true},
+    
+})
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const connect = require('./configs/db')
 const app = express()
 
+
+
 app.listen('2000', async ()=>{
     await connect()
     console.log('Listening on port 2000')
