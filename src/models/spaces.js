3012524f3b8b22da882190a4a3217f897e09cc9e
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-12-14 16:50:23 +0530

    spaces schema

--- src/models/spaces.js
+++ src/models/spaces.js
@@ -2,5 +2,13 @@ const mongoose = require('mongoose')
  
 module.exports = new mongoose.Schema({
     name : {type: String, required: true},
-    
+    type : {type: String, required: true},
+    images : [{type: String, required: true}],
+    body : {type: String, required: true},
+    rate : {type: Number, required: true},
+    rating : {type: Number, required: true},
+    area : {type: Number, required: true},
+    bedroom : {type: Number, required: true},
+    spaces : [{type: String, required: true}],
+    rules : [{type: String, required: true}],
 })
