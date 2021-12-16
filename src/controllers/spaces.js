ffd40104d99d4d22082b68cd7c0bd02598f94994
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-12-16 23:18:11 +0530

    db server

--- src/controllers/spaces.js
+++ src/controllers/spaces.js
@@ -7,5 +7,14 @@ router.get('', async (req, res)=>{
     const spaces = await Spaces.find().lean().exec()
     res.status(200).send(spaces)
 })
+// router.get('/single', async (req, res)=>{
+//     const spaces = await Spaces.find().lean().exec()
+//     res.status(200).send(spaces)
+// })
+
+router.post('', async (req, res)=>{
+    const space = await Spaces.create(req.body)
+    res.status(201).send(space)
+})
 
 model.exports = router
--- src/server.js
+++ src/server.js
@@ -5,11 +5,19 @@ app.use(express.json())
 app.set("view engine" , "ejs")
 app.use(express.static("public"))
 
+const Spaces = require('../models/spaces')
+
 app.get('/', (req, res)=>{
    return res.render('index.ejs', {})
 })
+
 app.get('/list', (req, res)=>{
    return res.render('list.ejs', {})
+})
+
+app.get('/single/:id', async (req, res)=>{
+    const space = await Spaces.findById(req.params.id)
+   return res.render('single.ejs', {space})
 })
 
 app.listen('2000', async ()=>{
