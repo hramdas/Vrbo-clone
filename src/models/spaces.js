d971b6930e7f8c99f236936ba5a64042a1d3e55a
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-12-16 23:09:32 +0530

    listejs, index ejs

--- src/models/spaces.js
+++ src/models/spaces.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
  
 module.exports = new mongoose.Schema({
     name : {type: String, required: true},
+    location : {type: String, required: true},
     type : {type: String, required: true},
     images : [{type: String, required: true}],
     body : {type: String, required: true},
